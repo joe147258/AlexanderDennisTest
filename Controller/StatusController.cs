30ead56d2f813dd87cbdde71532c165d73ec40b6
Joseph <joe147258@hotmail.co.uk>
2022-09-16 15:29:06 +0100

    Initial commit - start of form

---
+++ Controller/StatusController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlexanderDennisTest.Controller
+{
+    [Route("api/status")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Status()
+        {
+            return Ok("App is running.");
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,62 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+configureTimeSlot = () => {
+    // Get Date from date selector
+    let formDate = document.getElementById('formDate').value;
+    let date = new Date(formDate);
+    console.log(date.getDay());
+    // Select and Options DOM element
+    let timeSlotDiv = document.getElementById('formTimeSlotDiv');
+    let selectVar = document.createElement("select");
+    let optOne = document.createElement("option");
+    let optTwo = document.createElement("option");
+    let optThree = document.createElement("option");
+    
+    let pTag = document.createElement('p');
+
+    // Remove all children in DOM
+    timeSlotDiv.innerHTML = '';
+
+    // Check if date is selected
+    if (!formDate) {
+        pTag.innerHTML = "Please select a date.";
+        timeSlotDiv.append(pTag);
+        return;
+    }
+
+    // Check if weekday
+    if (date.getDay() === 6 || date.getDay() === 0) {
+        pTag.innerHTML = "Please select a week day.";
+        timeSlotDiv.append(pTag);
+        return;
+    }
+
+    switch (date.getDay()) {
+        case 1:
+            optOne.innerHTML = "8:00 - 10:00";
+            optOne.value = "8-10";
+
+            optTwo.innerHTML = "11:00 - 13:00";
+            optTwo.value = "11-13";
+
+            optThree.innerHTML = "14:00 - 16:00";
+            optThree.value = "14-16";
+        case 2:
+            console.log("Tues");
+        case 3:
+            console.log("Weds");
+        case 4:
+            console.log("Thurs");
+        case 5:
+            console.log("Fri");
+    }
+
+    selectVar.appendChild(optOne);
+    selectVar.appendChild(optTwo);
+    selectVar.appendChild(optThree);
+
+    timeSlotDiv.appendChild(selectVar);
+    
+}
